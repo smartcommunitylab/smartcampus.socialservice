b62cafdf8e5a8248f36fdaf3d1cf82d72d4595ac
mirkoperillo <mirko.perillo@gmail.com>
2014-02-06 14:45:49 +0100

    repository utility

---
+++ src/main/java/eu/trentorise/smartcampus/social/engine/utils/RepositoryUtils.java
@@ -0,0 +1,16 @@
+package eu.trentorise.smartcampus.social.engine.utils;
+
+public class RepositoryUtils {
+
+	public static String convertId(Long id) {
+		return id == null ? null : id.toString();
+	}
+
+	public static Long convertId(String id) throws IllegalArgumentException {
+		try {
+			return Long.valueOf(id);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(id + " not valid");
+		}
+	}
+}
