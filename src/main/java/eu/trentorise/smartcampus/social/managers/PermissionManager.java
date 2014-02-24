b9fcb34c4e5aaffe66b1f70f649ff92dedcee0ec
regolo85 <regolo85@gmail.com>
2014-02-24 15:18:04 +0100

    Update permissin manager for groups with null param case

--- src/main/java/eu/trentorise/smartcampus/social/managers/PermissionManager.java
+++ src/main/java/eu/trentorise/smartcampus/social/managers/PermissionManager.java
@@ -1,7 +1,9 @@
 package eu.trentorise.smartcampus.social.managers;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import eu.trentorise.smartcampus.social.engine.beans.Group;
 import eu.trentorise.smartcampus.social.engine.beans.Limit;
@@ -14,6 +16,8 @@ public class PermissionManager {
 	
 	Limit limit = null;
 	
+	private static final Logger logger = Logger.getLogger(SocialGroupManager.class);
+	
 	/**
 	 * Method checkGroupPermission: used to check if a user is the creator and the owner of a group
 	 * @param creatorId: id of the user to check
@@ -23,13 +27,18 @@ public class PermissionManager {
 	public boolean checkGroupPermission(String creatorId, String groupId){
 		boolean checked = false;
 		Group group = null;
-			group = groupManager.readGroup(groupId);
-			if(group == null){
-				return true;
-			}
-			if(group.getCreatorId().compareTo(creatorId) == 0){
-				checked = true;
-			}
+		if (!StringUtils.hasLength(creatorId)
+				|| !StringUtils.hasLength(groupId)) {
+			logger.error(String.format("Error in checking permission on creator 'null' or groupId 'null'."));
+			return checked;
+		}
+		group = groupManager.readGroup(groupId);
+		if (group == null) {
+			return true;
+		}
+		if (group.getCreatorId().compareTo(creatorId) == 0) {
+			checked = true;
+		}
 		return checked;
 	}
 }
