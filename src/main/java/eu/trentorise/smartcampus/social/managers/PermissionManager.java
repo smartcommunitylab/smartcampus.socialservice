c2de849ce9b4e79ab54832e011c549263c52f76b
mirkoperillo <mirko.perillo@gmail.com>
2014-02-24 15:49:23 +0100

    community check permissions

--- src/main/java/eu/trentorise/smartcampus/social/managers/PermissionManager.java
+++ src/main/java/eu/trentorise/smartcampus/social/managers/PermissionManager.java
@@ -5,28 +5,48 @@ import org.springframework.stereotype.Component;
 
 import eu.trentorise.smartcampus.social.engine.beans.Group;
 import eu.trentorise.smartcampus.social.engine.beans.Limit;
+import eu.trentorise.smartcampus.social.engine.model.SocialCommunity;
+import eu.trentorise.smartcampus.social.engine.repo.CommunityRepository;
+import eu.trentorise.smartcampus.social.engine.utils.RepositoryUtils;
 
 @Component
 public class PermissionManager {
 
 	@Autowired
 	SocialGroupManager groupManager;
-	
+
+	@Autowired
+	CommunityRepository communityRepo;
+
 	Limit limit = null;
-	
+
 	/**
-	 * Method checkGroupPermission: used to check if a user is the creator and the owner of a group
-	 * @param creatorId: id of the user to check
-	 * @param groupId: id of the group
+	 * Method checkGroupPermission: used to check if a user is the creator and
+	 * the owner of a group
+	 * 
+	 * @param creatorId
+	 *            : id of the user to check
+	 * @param groupId
+	 *            : id of the group
 	 * @return true if the user is the group creator of this group.
 	 */
-	public boolean checkGroupPermission(String creatorId, String groupId){
+	public boolean checkGroupPermission(String creatorId, String groupId) {
 		boolean checked = false;
 		Group group = null;
-			group = groupManager.readGroup(groupId);
-			if(group.getCreatorId().compareTo(creatorId) == 0){
-				checked = true;
-			}
+		group = groupManager.readGroup(groupId);
+		if (group.getCreatorId().compareTo(creatorId) == 0) {
+			checked = true;
+		}
 		return checked;
+	}
+
+	public boolean checkCommunityPermssion(String appId, String communityId) {
+		try {
+			SocialCommunity community = communityRepo.findOne(RepositoryUtils
+					.convertId(communityId));
+			return community != null && community.getAppId().equals(appId);
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
